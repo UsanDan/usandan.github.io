9de940d45da3f52d03282708c32b944f3e2933c8
UsanDan <usandan2@illinois.edu>
2025-03-05 11:28:02 -0600

    First websit commit.

---
+++ build_site.R
@@ -0,0 +1,5 @@
+#setwd just in case working directory was set to something else
+setwd("C:/Users/steph/OneDrive/Desktop/Personal Ltd/personal_website/usandan.github.io")
+
+#render site
+rmarkdown::render_site()
